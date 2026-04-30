dacfd1b7f99297d468b94593fe2ee28e7815e626
Rubelhasan29 <rubelhasanmd29@gmail.com>
2026-04-30 15:29:37 +0600

    added all the card with the image and title and grid 4 and fully responsive

--- src/app/components/BooksCard.jsx
+++ src/app/components/BooksCard.jsx
@@ -3,17 +3,18 @@ import Image from "next/image";
 const BooksCard = ({ data }) => {
 
     return (
-        <div className="card shadow-sm">
-            <figure>
-                <Image src={data.image_url} width={200} height={200} alt={data.title} loading="eager"></Image>
+        <div className="card bg-[#071117] p-2 overflow-hidden shadow-md hover:shadow-green-300/20 transition-all duration-300">
+            <figure className="h-90 w-full overflow-hidden">
+                <Image src={data.image_url} width={350} height={350} alt={data.title} loading="eager" className="w-full h-full object-cover hover:scale-110 transition-transform duration-500"></Image>
             </figure>
-            <div className="card-body">
+            <div className="card-body p-6 space-y-4">
                 <h2 className="card-title">{data.title}</h2>
-                <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
+                <p className="text-gray-300 line-clamp-2">{data.description}</p>
+                <div className="card-actions justify-start items-center">
+                    <h1 className="font-semibold text-xl ">⭐⭐⭐⭐⭐({data.available_quantity})</h1>
                 </div>
             </div>
+            <button className="btn btn-outline">View Details</button>
         </div>
     );
 };
--- src/app/components/HomePage.jsx
+++ src/app/components/HomePage.jsx
@@ -32,7 +32,7 @@ const HomePage = async () => {
                 <h1 className="text-4xl font-bold">Featured Books:</h1>
             </div>
 
-            <div className="grid grid-cols-4 gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-7">
                 {
                     data.map(d => <BooksCard key={d.id} data={d}></BooksCard>)
                 }
